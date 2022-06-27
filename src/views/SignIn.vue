4dc2aed06984e1baf699bcca7b9be2713bb4969e
yejine2 <yejine2@naver.com>
2022-06-28 00:35:28 +0900

    feat: Add Sign In

---
+++ src/views/SignIn.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container login">
+    <h1>로그인하세요</h1>
+    <div>
+      <input
+        v-model="email"
+        type="text"
+        placeholder="Email" />
+    </div>
+    <div>
+      <input
+        v-model="password"
+        type="text"
+        placeholder="Password" />
+    </div>
+    <button @click="signIn">
+      로그인
+    </button>
+    <div>
+      <RouterLink to="/join">
+        회원가입
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapStores } from 'pinia'
+import { useAuthStore } from '~/store/auth'
+
+export default {
+  data() {
+    return {
+      email: 'user_test1@gmail.com',
+      password: '1234qwer',
+    }
+  }, 
+  computed: {
+    ...mapStores(useAuthStore)
+  },  
+  methods: {
+    signIn() {
+        this.authStore.signIn({
+        email: this.email,
+        password: this.password
+      })
+      this.$router.push('/')
+    }
+  }
+}
+
+</script>
