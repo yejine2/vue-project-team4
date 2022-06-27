a2bead98cae4b0779bba1d2fc3361d64a243b915
yejine2 <yejine2@naver.com>
2022-06-28 00:33:51 +0900

    feat: Add Sign up

---
+++ src/views/SignUp.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <h1>회원가입</h1>
+    <div class="signup-container">
+      <div>
+        <input
+          v-model="email"
+          type="text"
+          placeholder="Email" />
+      </div>
+      <div>
+        <input
+          v-model="password"
+          maxlength="8"
+          type="text"
+          placeholder="Password" />
+      </div>
+      <div>
+        <input
+          v-model="displayName"
+          maxlength="20"
+          type="text"
+          placeholder="Name" />
+      </div>
+      <button @click="signUp">
+        가입하기
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapStores } from 'pinia'
+import { useAuthStore } from '~/store/auth'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      displayName: ''
+    }
+  },
+  computed: {
+    ...mapStores(useAuthStore)
+  },  
+  methods: {
+    signUp() {
+      this.authStore.signUp({
+        email: this.email,
+        password: this.password,
+        displayName: this.displayName
+      })
+    }
+  }
+}
+</script>
