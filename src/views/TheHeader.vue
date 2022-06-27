cbd87157906cffcde43016745d3f6a7601ebf5df
yejine2 <yejine2@naver.com>
2022-06-28 00:40:18 +0900

    feat: Add Header

---
+++ src/views/TheHeader.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="top-nav">
+    <RouterLink
+      to="/"
+      class="top-nav__link">
+      홈(임시)
+    </RouterLink>
+    <RouterLink
+      to="/my"
+      class="top-nav__link">
+      마이페이지
+    </RouterLink>
+    <!-- LogIn / LogOut -->
+    <template v-if="authStore.logInLoading">
+      Loading...
+    </template>
+    <template v-else-if="!authStore.user">
+      <RouterLink
+        to="/login"
+        class="top-nav__link">
+        로그인
+      </RouterLink>
+    </template>
+    <template v-else>
+      <RouterLink 
+        to="/login"
+        class="top-nav__link"
+        @click="authStore.signOut">
+        로그아웃
+      </RouterLink>
+    </template>
+  </div>
+  <div class="main-nav"></div>
+</template>
+
+<script>
+import { mapStores } from 'pinia'
+import { useAuthStore } from '~/store/auth'
+
+export default {
+  computed: {
+    ...mapStores(useAuthStore)
+  },
+  // watch: {
+  //   ['authStore.user'](user) {
+  //     console.log('user::::', user)
+  //   }
+  // },
+  // created() {
+  //   console.log(this.authStore.user)
+  // }
+}
+</script>
+
+<style scoped lang="scss">
+
+.top-nav{
+  display: flex;
+  justify-content: flex-end;
+  padding: 8px 40px;
+}
+.top-nav__link {
+  color: $color-black;
+  text-decoration: none;
+  font-size: 12px;
+  padding: .7rem;
+}
+
+</style>
