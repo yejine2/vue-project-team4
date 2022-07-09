a4cc85086cc7aad83dd7affd0d182e3f7d9ad79e
Mingu Kim <Mingu-Kim@outlook.com>
2022-07-10 00:27:21 +0900

    admin init

--- src/components/AdminPage.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>관리자 페이지입니다.</h1>
-</template>
---
+++ src/views/Admin.vue
@@ -0,0 +1,28 @@
+<template lang="">
+  <div>
+    <AdminNav />
+    <h1>전체 거래(판매) 내역 완료/취소 및 해제</h1>
+   
+    <button class="btn btn-warning">
+      제품 수정
+    </button>
+    <button class="btn btn-danger">
+      제품 삭제
+    </button>
+    <h3>단일 제품 상세 조회</h3>
+  </div>
+</template>
+
+<script>
+import AdminNav from '~/components/AdminNav.vue'
+
+export default {
+  components: {
+    AdminNav
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
