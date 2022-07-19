edd953f6f713159e0fa7ade6039c2f51b955cc6d
yejine2 <yejine2@naver.com>
2022-07-19 16:57:24 +0900

    feat: Add itemsWrap.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <Swiper />
+  <itemsWrap />
 </template>
 
 <script>
 import { mapStores } from 'pinia'
 import { useAuthStore } from '~/store/auth'
 import Swiper from '~/views/Swiper.vue'
+import itemsWrap from '~/views/layout/itemsWrap.vue'
 
 export default {
   components: {
-    Swiper
+    Swiper,
+    itemsWrap 
+    
   },
   computed: {
     ...mapStores(useAuthStore)
