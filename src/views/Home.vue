628664d6eabd442077c70594cff987aff4d3a05f
yejine2 <yejine2@naver.com>
2022-07-19 18:41:55 +0900

    feat: Add Home product list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <Swiper />
   <ItemsWrap />
+  <HomeProductList />
 </template>
 
 <script>
@@ -8,12 +9,13 @@ import { mapStores } from 'pinia'
 import { useAuthStore } from '~/store/auth'
 import Swiper from '~/views/Swiper.vue'
 import ItemsWrap from '~/views/layout/ItemsWrap.vue'
+import HomeProductList from '~/views/layout/HomeProductList.vue'
 
 export default {
   components: {
     Swiper,
-    ItemsWrap 
-    
+    ItemsWrap,
+    HomeProductList
   },
   computed: {
     ...mapStores(useAuthStore)
