f3ce3c0de89a63eefeb7248a79332c2cac5c7619
yejine2 <yejine2@naver.com>
2022-07-20 16:27:57 +0900

    fix: Modify component route in path '/my'

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ export default createRouter({
     },
     {
       path: '/my',
-      component: () => import('~/components/MyPage/My.vue'),
+      component: () => import('~/components/UserPage.vue'),
       meta: {
         // 로그인해야만 접속 할 수 있게 설정
         auth: true
@@ -31,8 +31,6 @@ export default createRouter({
     {
       path: '/admin/products',
       component: () => import('~/components/product/Products.vue')
-
-     
     },
     {
       path: '/admin/products/add',
